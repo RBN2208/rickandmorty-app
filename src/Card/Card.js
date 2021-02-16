7b6d8118bddd917664a651d9fdc51753080a8b51
Robin Steinhauser <rbn2208@gmail.com>
2021-02-16 14:04:53 +0100

    Styling changes

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,8 +3,10 @@ import './Card.css'
 export default function Card({ name, species, image }) {
   return (
     <section className="Card">
-      <h2>{name}</h2>
-      {species === 'Human' ? '👤' : '👽'}
+      <h2>
+        {species === 'Human' ? '👤' : '👽'}
+        {name}
+      </h2>
       <img className="Card__img" src={image} alt="" />
     </section>
   )
