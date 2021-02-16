9e19a1338baa90eebc946ce04d9c2e73a2a6fbc2
Robin Steinhauser <rbn2208@gmail.com>
2021-02-16 12:54:18 +0100

    Updatet Icon

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,8 +3,9 @@ import './Card.css'
 export default function Card({ name, species, image }) {
   return (
     <section className="Card">
-      <h2>{name}</h2> ({species})
-      <img src={image} alt="" />
+      <h2>{name}</h2>
+      {species === 'Human' ? '👤' : '👽'}
+      <img className="Card__img" src={image} alt="" />
     </section>
   )
 }
